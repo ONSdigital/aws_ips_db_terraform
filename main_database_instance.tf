d97edf16a2277c80ebf6c3257c2792d33a0412d3
elthorne <elinor.thorne@ext.ons.gov.uk>
2019-09-09 15:53:03 +0100

    main_database_instance.tf WIP. Ingress and Egress rules for Nat Instance are not correct.  Database security group needs a lot of work.

---
+++ main_database_instance.tf
@@ -0,0 +1,32 @@
+resource "aws_db_subnet_group" "db-subnet" {
+  name = "ips-db-subnet"
+  subnet_ids = "${aws_subnet.private_subnets.*.id}"
+
+  tags = {
+    Name = "ips-db-subnet",
+    app = "ips-db"
+  }
+}
+
+resource "aws_security_group" "db-sg" {
+  vpc_id      = aws_vpc.main_vpc.id
+  tags = merge(
+  local.private_vpc_resource_tags,
+    {
+      "Name" = "${local.common_name_prefix}-db-sg"
+    },
+  )
+
+  ingress {
+    from_port = 5432
+    protocol = "tcp"
+    to_port = 0
+  }
+
+  egress {
+    from_port = 0
+    protocol = ""
+    to_port = 0
+  }
+
+}
