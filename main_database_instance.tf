3eaaf28edc82ce9ad87277a2366cb73c80a6c227
Ashutosh Jindal <Ash.Jindal@ext.ons.gov.uk>
2019-09-17 10:41:31 +0100

    Create MYSQL DB Instance
    This commit introduces two new mandatory vars for DB credentials

--- main_database_instance.tf
+++ main_database_instance.tf
@@ -42,3 +42,30 @@ resource "aws_security_group_rule" "db_sg_from_nat" {
   // rules associated with security group:
   security_group_id = aws_security_group.db-sg.id
 }
+
+resource "aws_db_instance" "default" {
+  allocated_storage = 20
+  storage_type      = "gp2"
+  engine            = "mysql"
+  engine_version    = "5.7"
+  instance_class    = "db.t2.micro"
+
+  name       = local.common_name_prefix
+  identifier = local.common_name_prefix
+
+
+  username = var.db_user_name
+  password = var.db_password
+
+  parameter_group_name = "ips-mysql"
+
+  vpc_security_group_ids = [aws_security_group.db-sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.db-subnet.name
+
+  tags = merge(
+    local.module_common_tags,
+    {
+      "Name" = local.common_name_prefix
+    },
+  )
+}
